64007e7c93086b5e22b52dfbf3c7131dcb5be89d
JLopezz3 <lopezjerel@gmail.com>
2021-01-18 17:57:33 -0600

    build

---
+++ build/precache-manifest.accd8231d12a35d27121e2bebe821969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ef3a4c5a083d38fb1f188dee8e3830",
+    "url": "/index.html"
+  },
+  {
+    "revision": "386449d7128d33448dab",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386449d7128d33448dab",
+    "url": "/static/js/main.5980c019.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
